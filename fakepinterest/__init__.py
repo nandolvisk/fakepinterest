5c07ff5230fb09d0238c62034244f41741aadfac
Fernando Salerno <ffernando.salerno@gmail.com>
2024-06-24 02:08:20 -0300

    primeiro commit

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SECRET_KEY'] = '5c6a2b85e70fd9e773df53dc05133998'
+app.config['UPLOAD_FOLDER'] = 'static/fotos_posts'
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'homepage'
+
+from fakepinterest import routes
